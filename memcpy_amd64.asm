37039b53cc3fe8adce3cebce70bf626989a778d6
Marek Knápek <knapek.mar@gmail.com>
2026-06-22 22:25:00 +0200

    mkfw

--- memcpy_amd64.asm
+++ memcpy_amd64.asm
@@ -1,18 +1,16 @@
 .code
 
 memcpy proc
-	mov rax, rcx
-	test r8, r8
-	jz mk_done
-mk_loop:
-	mov r9b, [rdx]
-	mov [rcx], r9b
-	inc rdx
-	inc rcx
-	dec r8
-	jnz mk_loop
-mk_done:
-	ret
+         mov rax, rcx
+         test r8, r8
+         jz mk_done
+mk_loop: mov r9b, byte ptr [rdx]
+         mov byte ptr [rcx], r9b
+         inc rdx
+         inc rcx
+         dec r8
+         jnz mk_loop
+mk_done: ret
 memcpy endp
 
 end
